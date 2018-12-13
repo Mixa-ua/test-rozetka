0e1fdf344ed83cef82274e6b96fe671b5202648c
Mixa-ua <msdimon1992@gmail.com>
2018-12-13 21:51:54 +0200

    Оптиизировал код

--- src/test/java/nosulya/test/work/addCart.java
+++ src/test/java/nosulya/test/work/addCart.java
@@ -3,28 +3,28 @@ package nosulya.test.work;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-//import com.codeborne.selenide.Selectors;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-//import static com.codeborne.selenide.CollectionCondition.texts;
-//import static com.codeborne.selenide.Condition.attribute;
+
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+
 
 public class addCart {
 
 
-    @BeforeClass
-    public static void setup() {
-        Configuration.browser = "firefox";
+    @Before
+    public void setup() {
+        Configuration.browser = "chrome";
         open("https://rozetka.com.ua/");
     }
 
     @Test
     public void FindProduct() {
-        Configuration.timeout = 6000;
+        // Configuration.timeout = 6000;
         $(By.name("search")).shouldHave(Condition.attribute("placeholder", "Я ищу..."));
         $(By.name("search")).setValue("rx580");
         $(By.className("suggest-goods__link")).shouldHave(text ("Видеокарта ASUS MINING-RX580-4G-S SI"));
@@ -35,17 +35,19 @@ public class addCart {
 
     @Test
     public void AddToCart() {
+        $(By.name("search")).setValue("rx580").pressEnter();;
+        $(By.linkText ("Видеокарта ASUS MINING-RX580-4G-S SI")).click();
         $(By.className("toOrder")).click();
         //$(By.className("cart-title")).shouldHave(text("Корзина пуста"));
-         $(By.className("cart-title")).shouldHave(text("Вы добавили товар в корзину"));
+        $(By.className("cart-title")).shouldHave(text("Вы добавили товар в корзину"));
 
     }
 
     @Test
     public void ClickLogo() {
-        $(By.linkText ("Гарантия")).click();
+        $(By.linkText ("Контакты")).click();
         $(By.className("logo")).click();
-        $(By.tagName("title")).shouldHave(text("Интернет-магазин ROZETKA™: фототехника, видеотехника, аудиотехника, компьютеры и компьютерные комплектующие"));
+        $(By.className("main-auth__hading")).shouldHave(text("Добро пожаловать!"));
 
     }
 
